bda977c1543f6a19dd8808024983a8df931a3d5b
Peter Ivony <a11922002@univie.ac.at>
2023-10-28 10:29:43 +0200

    Initial commit

---
+++ include/LSSSolver.h
@@ -0,0 +1,14 @@
+//
+// Created by Peter Ivony on 27.10.23.
+//
+#ifndef HOMEWORK1_LSSSOLVER_H
+#define HOMEWORK1_LSSSOLVER_H
+
+#include <iostream>
+
+class LSSSolver{
+public:
+    virtual std::tuple<double, double> solve();
+};
+
+#endif //HOMEWORK1_LSSSOLVER_H
---
+++ src/HouseholderSolver.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Peter Ivony on 27.10.23.
+//
+#include "HouseholderSolver.h"
+
+std::tuple<double, double> HouseholderSolver::solve() {
+    return std::tuple(0.1, 0.2);
+}
