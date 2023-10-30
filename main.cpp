0da6f5116aa1ee318bb3d41f701f8e2c95a4b66f
Peter Ivony <a11922002@univie.ac.at>
2023-10-30 22:07:21 +0100

    Add class skeletons

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <cblas.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Define the matrix A and vector x
+    double A[] = {1.0, 2.0, 3.0, 4.0}; // 2x2 matrix
+    double x[] = {5.0, 6.0};          // 2-dimensional vector
+
+    // Create a vector to store the result
+    double b[2];
+
+    // Perform a matrix-vector multiplication using cblas
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, 2, 2, 1.0, A, 2, x, 1, 0.0, b, 1);
+
+    // Print the result
+    std::cout << "Matrix A:" << std::endl;
+    std::cout << "1.0 2.0" << std::endl;
+    std::cout << "3.0 4.0" << std::endl;
+
+    std::cout << "Vector x:" << std::endl;
+    std::cout << "5.0" << std::endl;
+    std::cout << "6.0" << std::endl;
+
+    std::cout << "Result b = A * x:" << std::endl;
+    std::cout << b[0] << std::endl;
+    std::cout << b[1] << std::endl;
+
+
     return 0;
 }
